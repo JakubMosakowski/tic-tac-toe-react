7c2cb22f9104579b1e71a1d7d7507f6861251b42
Jakub Mosakowski <mosakowskijakubsm@gmail.com>
2019-09-29 20:50:29 +0200

    Finished 4th additional task:
    "Add a toggle button that lets you sort the moves in either ascending or descending order."

--- src/PastMoves.js
+++ src/PastMoves.js
@@ -1,26 +1,66 @@
 import React from 'react';
 
 export default class PastMoves extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isAscending: true,
+            sortedHistory: this.props.history,
+            selectedIndex: this.props.clickedIndex
+        }
+    }
 
     render() {
-        const history = this.props.history
-        return (
-            history.map((step, move) => {
-                const desc = move ? 'Go to move #' + move : 'Go to start';
-                const squares = this.mapSquares(step.squares)
-                let className = this.getClassName(move);
+        const history = this.state.sortedHistory
+        const buttonText = this.state.isAscending ? "Ascending" : "Descending"
 
-                return (
-                    <li key={move}>
-                        <button className={className}
-                                onClick={() => this.props.onClick(move)}>{desc}</button>
-                        <table>
-                            {this.getTableContent(squares)}
-                        </table>
-                    </li>
-                )
-            })
+        return (
+            <div>
+                <div>
+                    <button onClick={() => this.handleSortClick()}>{buttonText}</button>
+                </div>
+                {
+                    this.renderPastMoves(history)
+                }
+            </div>
         )
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (JSON.stringify(prevProps) === JSON.stringify(this.props) && JSON.stringify(prevState) === JSON.stringify(this.state))
+            return
+
+        this.setState({
+            sortedHistory: this.getSortedHistory(),
+            selectedIndex: this.getSortedIndex()
+        })
+    }
+
+    renderPastMoves(history) {
+        return history.map((step, move) => {
+            const desc = this.getDesc(move);
+            const squares = this.mapSquares(step.squares)
+            let className = this.getClassName(move);
+
+            return (
+                <li key={move}>
+                    <button className={className}
+                            onClick={() => this.props.onClick(this.getMove(move))}>{desc}</button>
+                    <table>
+                        {this.getTableContent(squares)}
+                    </table>
+                </li>
+            )
+        })
+    }
+
+    getDesc(move) {
+        if (this.state.isAscending)
+            return move ? 'Go to move #' + move : 'Go to start'
+        else {
+            const history = this.props.history
+            return move === history.length - 1 ? 'Go to start' : 'Go to move #' + (history.length - 1 - move)
+        }
     }
 
     mapSquares(squares) {
@@ -33,7 +73,7 @@ export default class PastMoves extends React.Component {
     }
 
     getClassName(move) {
-        return move === this.props.clickedIndex ? "bolded" : ""
+        return move === this.state.selectedIndex ? "bolded" : ""
     }
 
     getTableContent(squares) {
@@ -58,5 +98,23 @@ export default class PastMoves extends React.Component {
         }
 
         return rows;
+    }
+
+    getSortedIndex() {
+        return this.state.isAscending ? this.props.clickedIndex : (this.props.history.length - 1 - this.props.clickedIndex)
+    }
+
+    getSortedHistory() {
+        return this.state.isAscending ? this.props.history : this.props.history.slice().reverse()
+    }
+
+    handleSortClick() {
+        this.setState({
+            isAscending: !this.state.isAscending
+        })
+    }
+
+    getMove(move) {
+        return this.state.isAscending ? move : (this.props.history.length - 1 - move)
     }
 }
